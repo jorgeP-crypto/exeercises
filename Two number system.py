26982103fac596deaf1ccdb508ecf5b5059f113e
jorgeP-crypto <56475278+jorgeP-crypto@users.noreply.github.com>
2020-08-15 07:28:41 +0000

    Update Two number system.py

--- Two number system.py
+++ Two number system.py
@@ -1,3 +1,6 @@
+#Exercise number one.
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
 def pairs(numbers, k):
     for i in range(len(numbers)):
         for a in range(i + 1, len(numbers)):
@@ -5,7 +8,8 @@ def pairs(numbers, k):
                 return True
     return False
 
+# I have allowed it to take k from the question from the user. 
 
 check_num = int(input("Enter the number you want to check:\n "))
 
-print(pairs([2, 5, 1, 6, 8, 10], check_num))
+print(pairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], check_num))
